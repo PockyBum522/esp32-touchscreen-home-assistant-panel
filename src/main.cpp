fbf03f54052caf640e248d55e45f4b4075559762
DSikes <david@sikespc.com>
2024-08-12 16:33:11 -0400

    Delete old code

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,6 @@
 // physically unplug the board again.
 
 
-
 constexpr uint8_t IO_EXPANDER_TOUCH_PANEL_RESET = 1;
 constexpr uint8_t IO_EXPANDER_LCD_BACKLIGHT = 2;
 constexpr uint8_t IO_EXPANDER_LCD_RESET = 3;
@@ -29,19 +28,7 @@ void setup()
 
     Serial.println("Initialize panel device");
     ESP_Panel *panel = new ESP_Panel();
-
-
     delay(150);
-
-    // uint8_t write_buf = 0x01;
-    // i2c_master_write_to_device(I2C_NUM_0, 0x24, &write_buf, 1, 500 / portTICK_PERIOD_MS);
-    // ESP_LOGI(TAG,"0x48 0x01 ret is %d",ret);
-    // write_buf = 0x0E;
-    // i2c_master_write_to_device(I2C_NUM_0, 0x38, &write_buf, 1, 500 / portTICK_PERIOD_MS);
-    // ESP_LOGI(TAG,"0x70 0x00 ret is %d",ret);
-
-
-
     panel->init();
     delay(150);
 #if LVGL_PORT_AVOID_TEAR
