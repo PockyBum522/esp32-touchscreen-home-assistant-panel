fa7e5ba2b8e5d0d389b94935bc989c64933afae8
DSikes <pockybum522@gmail.com>
2024-08-13 17:38:00 -0400

    WIP attempting to get board upload in line with simulations

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,98 @@ constexpr uint8_t IO_EXPANDER_LCD_RESET = 3;
 constexpr uint8_t IO_EXPANDER_SD_CS = 4;
 constexpr uint8_t IO_EXPANDER_USB_SELECT = 5;
 
+static lv_obj_t * garageButton;
+
+
+void make_background_dark(void)
+{
+    /*Change the active screen's background color*/
+    lv_obj_set_style_bg_color(lv_screen_active(), lv_color_hex(0x111111), LV_PART_MAIN);
+}
+
+void make_hello_label(void)
+{
+    /*Create a white label, set its text and align it to the center*/
+    lv_obj_t * label = lv_label_create(lv_screen_active());
+    lv_label_set_text(label, "Hello world");
+    lv_obj_set_style_text_color(lv_screen_active(), lv_color_hex(0xEEEEEE), LV_PART_MAIN);
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+}
+
+void static event_garage_door_button(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+    lv_obj_t * label = lv_event_get_user_data(e);
+
+    switch(code)
+    {
+        case LV_EVENT_PRESSED:
+            printf("The last button event:\nLV_EVENT_PRESSED");
+        break;
+
+        case LV_EVENT_CLICKED:
+            printf("The last button event:\nLV_EVENT_CLICKED");
+        break;
+
+        case LV_EVENT_LONG_PRESSED:
+            printf("The last button event:\nLV_EVENT_LONG_PRESSED");
+        break;
+
+        case LV_EVENT_LONG_PRESSED_REPEAT:
+            printf("The last button event:\nLV_EVENT_LONG_PRESSED_REPEAT");
+        break;
+
+        default:
+            break;
+    }
+}
+
+void make_garage_door_open_button()
+{
+    garageButton = lv_button_create(lv_screen_active());
+    lv_obj_set_size(garageButton, 140, 140);
+    lv_obj_center(garageButton);
+
+    lv_obj_t * btn_label = lv_label_create(garageButton);
+    lv_label_set_text(btn_label, "Garage Door");
+    lv_obj_center(btn_label);
+
+    lv_obj_add_event_cb(garageButton, event_garage_door_button, LV_EVENT_ALL, NULL);
+}
+
+void make_garage_door_button_with_style(void)
+{
+    static const lv_style_prop_t props[] = {LV_STYLE_BG_COLOR, LV_STYLE_BORDER_COLOR, LV_STYLE_BORDER_WIDTH, 0};
+
+    /* A default transition, make it fast (100ms) and start with some delay (200 ms)*/
+    static lv_style_transition_dsc_t trans_def;
+    lv_style_transition_dsc_init(&trans_def, props, lv_anim_path_linear, 400, 3000, NULL);
+
+    /* A special transition when going to pressed state
+     * Make it slow (500 ms) but start  without delay*/
+    static lv_style_transition_dsc_t trans_pr;
+    lv_style_transition_dsc_init(&trans_pr, props, lv_anim_path_linear, 4000, 0, NULL);
+
+    static lv_style_t style_def;
+    lv_style_init(&style_def);
+    lv_style_set_transition(&style_def, &trans_def);
+
+    static lv_style_t style_pr;
+    lv_style_init(&style_pr);
+    lv_style_set_bg_color(&style_pr, lv_palette_main(LV_PALETTE_RED));
+    lv_style_set_border_width(&style_pr, 66);
+    lv_style_set_border_color(&style_pr, lv_palette_darken(LV_PALETTE_RED, 100));
+    lv_style_set_transition(&style_pr, &trans_pr);
+
+    make_garage_door_open_button();
+
+    /*Create an object with the new style_pr*/
+    lv_obj_add_style(garageButton, &style_def, 0);
+    lv_obj_add_style(garageButton, &style_pr, LV_STATE_PRESSED);
+
+    lv_obj_center(garageButton);
+}
+
 void setup()
 {
     String title = "LVGL porting example";
@@ -62,7 +154,17 @@ void setup()
     lvgl_port_unlock();
 
     Serial.println(title + " end");
+
+    lv_init();
+
+    /*Output prompt information to the console, you can also use printf() to print directly*/
+    LV_LOG_USER("LVGL initialization completed!");
+
+    make_background_dark();
+    make_hello_label();
+    make_garage_door_button_with_style();
 }
+
 
 void loop()
 {
