83e651bcc432f6c64e4fdb3299770d691db6301a
DSikes <david@sikespc.com>
2024-08-13 08:10:25 -0400

    Add codeblocks LVGL environment for faster GUI creation on Windows

---
+++ codeblocks_simulator_environment/main.c
@@ -0,0 +1,77 @@
+
+/**
+ * @file main
+ *
+ */
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "lvgl/lvgl.h"
+#include "lvgl/demos/lv_demos.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ *  STATIC PROTOTYPES
+ **********************/
+
+/**********************
+ *  STATIC VARIABLES
+ **********************/
+
+/**********************
+ *      MACROS
+ **********************/
+
+/**********************
+ *   GLOBAL FUNCTIONS
+ **********************/
+
+ void set_background_dark(void)
+{
+    /*Change the active screen's background color*/
+    lv_obj_set_style_bg_color(lv_screen_active(), lv_color_hex(0x111111), LV_PART_MAIN);
+}
+
+ void make_hello_label(void)
+{
+    /*Create a white label, set its text and align it to the center*/
+    lv_obj_t * label = lv_label_create(lv_screen_active());
+    lv_label_set_text(label, "Hello world");
+    lv_obj_set_style_text_color(lv_screen_active(), lv_color_hex(0xEEEEEE), LV_PART_MAIN);
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+}
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR szCmdLine, int nCmdShow)
+{
+    /*Initialize LVGL*/
+    lv_init();
+
+    /*Initialize the HAL for LVGL*/
+    lv_display_t * display = lv_windows_create_display(title, 800, 480, 100, FALSE, FALSE);
+    lv_windows_acquire_pointer_indev(display);
+
+    /*Output prompt information to the console, you can also use printf() to print directly*/
+    LV_LOG_USER("LVGL initialization completed!");
+
+    set_background_dark();
+    make_hello_label();
+
+    while(1) {
+        /* Periodically call the lv_task handler.
+         * It could be done in a timer interrupt or an OS task too.*/
+        lv_task_handler();
+        usleep(5000);       /*Just to let the system breath*/
+    }
+    return 0;
+}
