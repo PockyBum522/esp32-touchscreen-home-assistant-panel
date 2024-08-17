ae3ee22b5612f2d20a4838f8f502b6b3805d1b93
DSikes <david@sikespc.com>
2024-08-17 08:26:57 -0400

    Reintegrated old main.cpp

--- platformio-touchscreen-src/src/main.cpp
+++ platformio-touchscreen-src/src/main.cpp
@@ -43,12 +43,26 @@
  */
 
 #include <Arduino.h>
+#include <Secrets/Secrets.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <ESP32Time.h>
+#include <ElegantOTA.h>
 #include <ESP_Panel_Library.h>
+#include <lvgl.h>
+#include <cstdint>
+#include <src/widgets/lv_label.h>
 #include <ESP_IOExpander_Library.h>
 
-#include <lvgl.h>
+#include "Logging/Logger.h"
 #include "lvgl_port_v8.h"
-#include <demos/lv_demos.h>
+
+
+bool m_debugSerialOn = true;
+
+String m_versionNumber = "v01";
+String m_applicationName = "Den Touchscreen";
+
 
 // Extend IO Pin define
 #define TP_RST 1
@@ -56,7 +70,6 @@
 #define LCD_RST 3
 #define SD_CS 4
 #define USB_SEL 5
-
 
 // LVGL stuff
 static lv_style_t style_text_muted;
@@ -72,18 +85,31 @@ static lv_obj_t * denLightsButton;
 static lv_obj_t * tvButton;
 
 
-/**
-/* To use the built-in examples and demos of LVGL uncomment the includes below respectively.
- * You also need to copy `lvgl/examples` to `lvgl/src/examples`. Similarly for the demos `lvgl/demos` to `lvgl/src/demos`.
- */
-#include <demos/lv_demos.h>
-#include "demos/widgets/lv_demo_widgets.h"
-#include <examples/lv_examples.h>
+// Dependency setup
+auto logger = *new Logger(Information, &m_debugSerialOn);
+WebServer server(80);
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
+ESP32Time rtc(0);
 
+
+void connectWifi();
+void connectMqtt();
+void mqttCallback(char *topic, byte *payload, unsigned int length);
 void lvglInitComponents();
 
 void setup()
 {
+    if (m_debugSerialOn)
+    {
+        Serial.begin(115200);
+
+        Serial.print(m_applicationName + " start - ");
+        Serial.println(m_versionNumber);
+
+        Serial.println("Initialize panel device");
+    }
+
     String title = "LVGL porting example";
 
     Serial.begin(115200);
@@ -168,6 +194,144 @@ void loop()
 }
 
 
+
+
+void connectWifi()
+{
+    if (m_debugSerialOn)
+    {
+        Serial.print("Connecting to wifi");
+        //Serial.println(ssid);
+    }
+
+    WiFi.begin(SECRETS::WifiSsid, SECRETS::WifiPsk);
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+
+        if (m_debugSerialOn)
+        {
+            Serial.print(".");
+        }
+    }
+
+    if (m_debugSerialOn)
+    {
+        Serial.println("");
+        Serial.println("WiFi connected.");
+        Serial.println("IP address: ");
+        Serial.println(WiFi.localIP());
+    }
+}
+
+void connectMqtt()
+{
+
+    mqttClient.setServer(SECRETS::MqttBroker, SECRETS::MqttPort);
+    mqttClient.setCallback(mqttCallback);
+
+    while (!mqttClient.connected())
+    {
+        server.handleClient();
+        ElegantOTA.loop();
+
+        String client_id = "esp32-client-";
+        client_id += String(WiFi.macAddress());
+
+        if (m_debugSerialOn)
+            Serial.printf("The client %s connects to the public MQTT broker\n", client_id.c_str());
+
+        if (mqttClient.connect(client_id.c_str(), SECRETS::MqttUsername, SECRETS::MqttPassword))
+        {
+            if (m_debugSerialOn)
+                Serial.println("Public EMQX MQTT broker connected");
+        }
+        else
+        {
+            if (m_debugSerialOn)
+            {
+                Serial.print("failed with state ");
+                Serial.print(mqttClient.state());
+            }
+
+            delay(100);
+        }
+    }
+
+    String subscribeMessage = "Subscribing to: ";
+    mqttClient.publish(SECRETS::MqttTopicGeneralStatus, subscribeMessage.c_str());
+
+    String helloMessage = m_applicationName + " ESP32 up! ";
+    helloMessage += m_versionNumber;
+    helloMessage += " @ ";
+    helloMessage += String(WiFi.localIP().toString());
+
+    mqttClient.publish(SECRETS::MqttTopicGeneralStatus, helloMessage.c_str());
+    mqttClient.publish(SECRETS::MqttTopicDeviceStatus, helloMessage.c_str());
+}
+
+String getIncomingPayloadAsString(uint8_t *payload, unsigned int payloadLength)
+{
+    String payloadStr = "";
+
+    // Convert payload to string
+    for (int i = 0; i < payloadLength; i++)
+    {
+        payloadStr += ((char)payload[i]);
+    }
+
+    return payloadStr;
+}
+
+String getIncomingTopicAsString(char *topic)
+{
+    String topicStr = "";
+
+    // Convert topic to string
+    for (int i = 0; i < strlen(topic); i++)
+    {
+        topicStr += ((char)topic[i]);
+    }
+
+    return topicStr;
+}
+
+void mqttCallback(char *topic, byte *payload, unsigned int length)
+{
+    if (m_debugSerialOn)
+    {
+        Serial.print("Message arrived in topic: ");
+        Serial.println(topic);
+
+        Serial.print("Message:");
+    }
+
+    String topicStr = getIncomingTopicAsString(topic);
+    String payloadStr = getIncomingPayloadAsString(payload, length);
+
+    String newMessage = "New message on: '";
+    newMessage += topicStr;
+    newMessage += "' with payload of: '";
+    newMessage += payloadStr;
+    newMessage += "'";
+
+    mqttClient.publish(SECRETS::MqttTopicGeneralStatus,  newMessage.c_str());
+    mqttClient.publish(SECRETS::MqttTopicDeviceStatus,  newMessage.c_str());
+
+    if (topicStr == SECRETS::MqttTopicGeneralStatus)
+    {
+        if (payloadStr == "info")
+        {
+            String incomingMessage = payloadStr;
+            incomingMessage += " <= New";
+
+            mqttClient.publish(SECRETS::MqttTopicDeviceStatus,  incomingMessage.c_str());
+        }
+    }
+}
+
+
 void make_background_dark()
 {
     /*Change the active screen's background color*/
@@ -176,7 +340,44 @@ void make_background_dark()
 
 void static event_button(lv_event_t * e)
 {
-    return;
+    lv_event_code_t code = lv_event_get_code(e);
+    //lv_obj_t * label = lv_event_get_user_data(e);
+
+    // Cast a dynamically allocated string to 'void*'.
+    void *vp = static_cast<void*>(new std::string("it's easy to break stuff like this!"));
+
+    // Then, in the function that's using the UserEvent:
+    // Cast it back to a string pointer.
+    auto *sp = static_cast<std::string*>(lv_event_get_user_data(e));
+
+    // You could use 'sp' directly, or this, which does a copy.
+    std::string s = *sp;
+
+    switch(code)
+    {
+        case LV_EVENT_PRESSED:
+            mqttClient.publish(SECRETS::MqttTopicButtonsPressed,  (s + "_pressed").c_str());
+        break;
+
+        case LV_EVENT_CLICKED:
+            // Don't think we need this as pressed always fires
+                //mqttClient.publish(SECRETS::MqttTopicButtonsPressed, "den_lights_button_pressed");
+                    break;
+
+        case LV_EVENT_LONG_PRESSED:
+            mqttClient.publish(SECRETS::MqttTopicButtonsPressed, (s + "_long_pressed").c_str());
+        break;
+
+        case LV_EVENT_LONG_PRESSED_REPEAT:
+            mqttClient.publish(SECRETS::MqttTopicButtonsPressed, (s + "_long_pressed_repeat").c_str());
+        break;
+
+        default:
+            break;
+    }
+
+    // Don't forget to destroy the memory that you've allocated.
+    // delete sp;
 }
 
 void make_garage_door_button_with_style()
