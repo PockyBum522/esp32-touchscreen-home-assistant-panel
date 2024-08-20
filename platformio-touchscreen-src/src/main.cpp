c7e70634bcedb5ad4b8250d0c72dd6f7a2684737
DSikes <david@sikespc.com>
2024-08-20 08:13:51 -0400

    Taking code down to just LVGL for testing

--- platformio-touchscreen-src/src/main.cpp
+++ platformio-touchscreen-src/src/main.cpp
@@ -1,16 +1,10 @@
 #include <Arduino.h>
-#include <Secrets/Secrets.h>
-#include <WiFi.h>
-#include <PubSubClient.h>
-#include <ESP32Time.h>
-#include <ElegantOTA.h>
 #include <ESP_Panel_Library.h>
 #include <lvgl.h>
 #include <cstdint>
 #include <src/widgets/lv_label.h>
 #include <ESP_IOExpander_Library.h>
 
-#include "Logging/Logger.h"
 #include "lvgl_port_v8.h"
 
 bool m_debugSerialOn = true;
@@ -37,13 +31,6 @@ static lv_obj_t * garageButton;
 static lv_obj_t * denLightsButton;
 static lv_obj_t * tvButton;
 
-// Dependency setup
-auto logger = *new Logger(Information, &m_debugSerialOn);
-WebServer server(80);
-WiFiClient espClient;
-PubSubClient mqttClient(espClient);
-ESP32Time rtc(0);
-
 
 void connectWifi();
 void connectMqtt();
@@ -62,7 +49,6 @@ void setup()
         Serial.println("Initialize panel device");
     }
 
-    Serial.begin(115200);
      pinMode(GPIO_INPUT_IO_4, OUTPUT);
 
     /**
@@ -119,144 +105,6 @@ void loop()
 }
 
 
-
-
-void connectWifi()
-{
-    if (m_debugSerialOn)
-    {
-        Serial.print("Connecting to wifi");
-        //Serial.println(ssid);
-    }
-
-    WiFi.begin(SECRETS::WifiSsid, SECRETS::WifiPsk);
-
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(500);
-
-        if (m_debugSerialOn)
-        {
-            Serial.print(".");
-        }
-    }
-
-    if (m_debugSerialOn)
-    {
-        Serial.println("");
-        Serial.println("WiFi connected.");
-        Serial.println("IP address: ");
-        Serial.println(WiFi.localIP());
-    }
-}
-
-void connectMqtt()
-{
-
-    mqttClient.setServer(SECRETS::MqttBroker, SECRETS::MqttPort);
-    mqttClient.setCallback(mqttCallback);
-
-    while (!mqttClient.connected())
-    {
-        server.handleClient();
-        ElegantOTA.loop();
-
-        String client_id = "esp32-client-";
-        client_id += String(WiFi.macAddress());
-
-        if (m_debugSerialOn)
-            Serial.printf("The client %s connects to the public MQTT broker\n", client_id.c_str());
-
-        if (mqttClient.connect(client_id.c_str(), SECRETS::MqttUsername, SECRETS::MqttPassword))
-        {
-            if (m_debugSerialOn)
-                Serial.println("Public EMQX MQTT broker connected");
-        }
-        else
-        {
-            if (m_debugSerialOn)
-            {
-                Serial.print("failed with state ");
-                Serial.print(mqttClient.state());
-            }
-
-            delay(100);
-        }
-    }
-
-    String subscribeMessage = "Subscribing to: ";
-    mqttClient.publish(SECRETS::MqttTopicGeneralStatus, subscribeMessage.c_str());
-
-    String helloMessage = m_applicationName + " ESP32 up! ";
-    helloMessage += m_versionNumber;
-    helloMessage += " @ ";
-    helloMessage += String(WiFi.localIP().toString());
-
-    mqttClient.publish(SECRETS::MqttTopicGeneralStatus, helloMessage.c_str());
-    mqttClient.publish(SECRETS::MqttTopicDeviceStatus, helloMessage.c_str());
-}
-
-String getIncomingPayloadAsString(uint8_t *payload, unsigned int payloadLength)
-{
-    String payloadStr = "";
-
-    // Convert payload to string
-    for (int i = 0; i < payloadLength; i++)
-    {
-        payloadStr += ((char)payload[i]);
-    }
-
-    return payloadStr;
-}
-
-String getIncomingTopicAsString(char *topic)
-{
-    String topicStr = "";
-
-    // Convert topic to string
-    for (int i = 0; i < strlen(topic); i++)
-    {
-        topicStr += ((char)topic[i]);
-    }
-
-    return topicStr;
-}
-
-void mqttCallback(char *topic, byte *payload, unsigned int length)
-{
-    if (m_debugSerialOn)
-    {
-        Serial.print("Message arrived in topic: ");
-        Serial.println(topic);
-
-        Serial.print("Message:");
-    }
-
-    String topicStr = getIncomingTopicAsString(topic);
-    String payloadStr = getIncomingPayloadAsString(payload, length);
-
-    String newMessage = "New message on: '";
-    newMessage += topicStr;
-    newMessage += "' with payload of: '";
-    newMessage += payloadStr;
-    newMessage += "'";
-
-    mqttClient.publish(SECRETS::MqttTopicGeneralStatus,  newMessage.c_str());
-    mqttClient.publish(SECRETS::MqttTopicDeviceStatus,  newMessage.c_str());
-
-    if (topicStr == SECRETS::MqttTopicGeneralStatus)
-    {
-        if (payloadStr == "info")
-        {
-            String incomingMessage = payloadStr;
-            incomingMessage += " <= New";
-
-            mqttClient.publish(SECRETS::MqttTopicDeviceStatus,  incomingMessage.c_str());
-        }
-    }
-}
-
-
 void make_background_dark()
 {
     /*Change the active screen's background color*/
@@ -265,8 +113,6 @@ void make_background_dark()
 
 void static event_button(lv_event_t * e)
 {
-    return;
-
     lv_event_code_t code = lv_event_get_code(e);
     //lv_obj_t * label = lv_event_get_user_data(e);
 
@@ -283,7 +129,8 @@ void static event_button(lv_event_t * e)
     switch(code)
     {
         case LV_EVENT_PRESSED:
-            mqttClient.publish(SECRETS::MqttTopicButtonsPressed,  (s + "_pressed").c_str());
+            Serial.println("Event: Button pressed");
+            //mqttClient.publish(SECRETS::MqttTopicButtonsPressed,  (s + "_pressed").c_str());
         break;
 
         case LV_EVENT_CLICKED:
@@ -292,11 +139,13 @@ void static event_button(lv_event_t * e)
                     break;
 
         case LV_EVENT_LONG_PRESSED:
-            mqttClient.publish(SECRETS::MqttTopicButtonsPressed, (s + "_long_pressed").c_str());
+            Serial.println("Event: Button LONG pressed");
+            //mqttClient.publish(SECRETS::MqttTopicButtonsPressed, (s + "_long_pressed").c_str());
         break;
 
         case LV_EVENT_LONG_PRESSED_REPEAT:
-            mqttClient.publish(SECRETS::MqttTopicButtonsPressed, (s + "_long_pressed_repeat").c_str());
+            Serial.println("Event: Button long pressed (repeat)");
+            //mqttClient.publish(SECRETS::MqttTopicButtonsPressed, (s + "_long_pressed_repeat").c_str());
         break;
 
         default:
