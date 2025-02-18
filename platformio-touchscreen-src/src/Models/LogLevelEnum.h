9b086461f92124fcdc09f27188f68f90fff1e79f
David S <1970959+PockyBum522@users.noreply.github.com>
2025-02-18 09:40:55 -0500

    Adding back in old code to test with new high_perf platform files

---
+++ platformio-touchscreen-src/src/Models/LogLevelEnum.h
@@ -0,0 +1,15 @@
+#ifndef LOGLEVELENUM_H
+#define LOGLEVELENUM_H
+
+enum LogLevel
+{
+    Uninitialized = 0,
+    Fatal = 1,
+    Error = 2,
+    Warning = 3,
+    Information = 4,
+    Debug = 5,
+    LoggingDisabled = 6
+};
+
+#endif //LOGLEVELENUM_H
