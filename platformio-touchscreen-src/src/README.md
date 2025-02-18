9d0c86caf93826e1103b25ba72a92a3d07c8d4db
David S <1970959+PockyBum522@users.noreply.github.com>
2025-02-18 11:08:56 -0500

    RGB test example working

--- platformio-touchscreen-src/src/README.md
+++ platformio-touchscreen-src/src/README.md
@@ -1,40 +1,36 @@
-# LVGL Porting Example
+| Supported ESP SoCs | ESP32-S3 |
+| ------------------ | -------- |
 
-The example demonstrates how to port LVGL (v8.3.x). And for RGB LCD, it can enable the avoid tearing fucntion.
+| Supported LCD Controllers | ST7262 |
+| ------------------------- | ------ |
 
-## How to Use
+# Single RGB LCD Example
 
-To use this example, please firstly install the following dependent libraries:
+The example demonstrates how to develop different model LCDs with RGB (without 3-wire SPI) interface using standalone drivers and test them by displaying color bars.
 
-- lvgl (v8.3.x)
+## How to use
 
-Then follow the steps below to configure:
-
-1. For **ESP32_Display_Panel**:
-
-    - [Configure drivers](../../../README.md#configuring-drivers) if needed.
-    - If using a supported development board, follow the [steps](../../../../README.md#using-supported-development-boards) to configure it.
-    - If using a custom board, follow the [steps](../../../../README.md#using-custom-development-boards) to configure it.
-
-2. Follow the [steps](../../../../README.md#configuring-lvgl) to configure the **lvgl**.
-3. Modify the macros in the [lvgl_port_v8.h](./lvgl_port_v8.h) file to configure the LVGL porting parameters.
-4. Navigate to the `Tools` menu in the Arduino IDE to choose a ESP board and configure its parameters, please refter to [Configuring Supported Development Boards](../../../../README.md#configuring-supported-development-boards)
-5. Verify and upload the example to your ESP board.
+1. [Configure drivers](../../../README.md#configuring-drivers) if needed.
+2. Modify the macros in the example to match the parameters according to your hardware.
+3. Navigate to the `Tools` menu in the Arduino IDE to choose a ESP board and configure its parameters, please refter to [Configuring Supported Development Boards](../../../README.md#configuring-supported-development-boards)
+4. Verify and upload the example to your ESP board.
 
 ## Serial Output
 
-```bash
+```
 ...
-LVGL porting example start
-Initialize panel device
-Initialize LVGL
-Create UI
-LVGL porting example end
-IDLE loop
-IDLE loop
+RGB LCD example start
+Create RGB LCD bus
+Create LCD device
+Draw color bar from top left to bottom right, the order is B - G - R
+RGB LCD example end
+RGB refresh rate: 0
+RGB refresh rate: 0
+RGB refresh rate: 31
+RGB refresh rate: 31
 ...
 ```
 
 ## Troubleshooting
 
-Please check the [FAQ](../../../../README.md#faq) first to see if the same question exists. If not, please create a [Github issue](https://github.com/esp-arduino-libs/ESP32_Display_Panel/issues). We will get back to you as soon as possible.
+Please check the [FAQ](../../../README.md#faq) first to see if the same question exists. If not, please create a [Github issue](https://github.com/esp-arduino-libs/ESP32_Display_Panel/issues). We will get back to you as soon as possible.
--- platformio-touchscreen-src/src/main.cpp
+++ platformio-touchscreen-src/src/main.cpp
@@ -1,38 +1,34 @@
 /**
- * # LVGL Porting Example
+ * | Supported ESP SoCs | ESP32-S3 |
+ * | ------------------ | -------- |
  *
- * The example demonstrates how to port LVGL (v8.3.x). And for RGB LCD, it can enable the avoid tearing fucntion.
+ * | Supported LCD Controllers | ST7262 |
+ * | ------------------------- | ------ |
  *
- * ## How to Use
+ * # Single RGB LCD Example
  *
- * To use this example, please firstly install the following dependent libraries:
+ * The example demonstrates how to develop different model LCDs with RGB (without 3-wire SPI) interface using standalone drivers and test them by displaying color bars.
  *
- * - lvgl (v8.3.x)
+ * ## How to use
  *
- * Then follow the steps below to configure:
- *
- * 1. For **ESP32_Display_Panel**:
- *
- *    - [Configure drivers](https://github.com/esp-arduino-libs/ESP32_Display_Panel#configuring-drivers) if needed.
- *    - If using a supported development board, follow the [steps](https://github.com/esp-arduino-libs/ESP32_Display_Panel#using-supported-development-boards) to configure it.
- *    - If using a custom board, follow the [steps](https://github.com/esp-arduino-libs/ESP32_Display_Panel#using-custom-development-boards) to configure it.
- *
- * 2. Follow the [steps](https://github.com/esp-arduino-libs/ESP32_Display_Panel#configuring-lvgl) to configure the **lvgl**.
- * 3. Modify the macros in the [lvgl_port_v8.h](./lvgl_port_v8.h) file to configure the LVGL porting parameters.
- * 4. Navigate to the `Tools` menu in the Arduino IDE to choose a ESP board and configure its parameters, please refter to [Configuring Supported Development Boards](https://github.com/esp-arduino-libs/ESP32_Display_Panel#configuring-supported-development-boards)
- * 5. Verify and upload the example to your ESP board.
+ * 1. [Configure drivers](https://github.com/esp-arduino-libs/ESP32_Display_Panel#configuring-drivers) if needed.
+ * 2. Modify the macros in the example to match the parameters according to your hardware.
+ * 3. Navigate to the `Tools` menu in the Arduino IDE to choose a ESP board and configure its parameters, please refter to [Configuring Supported Development Boards](https://github.com/esp-arduino-libs/ESP32_Display_Panel#configuring-supported-development-boards)
+ * 4. Verify and upload the example to your ESP board.
  *
  * ## Serial Output
  *
- * ```bash
+ * ```
  * ...
- * LVGL porting example start
- * Initialize panel device
- * Initialize LVGL
- * Create UI
- * LVGL porting example end
- * IDLE loop
- * IDLE loop
+ * RGB LCD example start
+ * Create RGB LCD bus
+ * Create LCD device
+ * Draw color bar from top left to bottom right, the order is B - G - R
+ * RGB LCD example end
+ * RGB refresh rate: 0
+ * RGB refresh rate: 0
+ * RGB refresh rate: 31
+ * RGB refresh rate: 31
  * ...
  * ```
  *
@@ -44,70 +40,163 @@
 
 #include <Arduino.h>
 #include <ESP_Panel_Library.h>
-#include <lvgl.h>
-#include "lvgl_port_v8.h"
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////// Please update the following configuration according to your LCD spec //////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /**
-/* To use the built-in examples and demos of LVGL uncomment the includes below respectively.
- * You also need to copy `lvgl/examples` to `lvgl/src/examples`. Similarly for the demos `lvgl/demos` to `lvgl/src/demos`.
+ * Currently, the library supports the following RGB (without 3-wire SPI) LCDs:
+ *      - ST7262
  */
-#include <demos/lv_demos.h>
-#include <examples/lv_examples.h>
+#define EXAMPLE_LCD_NAME                        ST7262
+#define EXAMPLE_LCD_WIDTH                       (800)
+#define EXAMPLE_LCD_HEIGHT                      (480)
+#define EXAMPLE_LCD_COLOR_BITS                  (16)
+#define EXAMPLE_LCD_RGB_DATA_WIDTH              (16)
+#define EXAMPLE_LCD_RGB_TIMING_FREQ_HZ          (16 * 1000 * 1000)
+#define EXAMPLE_LCD_RGB_TIMING_HPW              (40)
+#define EXAMPLE_LCD_RGB_TIMING_HBP              (40)
+#define EXAMPLE_LCD_RGB_TIMING_HFP              (48)
+#define EXAMPLE_LCD_RGB_TIMING_VPW              (23)
+#define EXAMPLE_LCD_RGB_TIMING_VBP              (32)
+#define EXAMPLE_LCD_RGB_TIMING_VFP              (13)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////// Please update the following configuration according to your board spec ////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#define EXAMPLE_LCD_PIN_NUM_RGB_DISP            (-1)
+#define EXAMPLE_LCD_PIN_NUM_RGB_VSYNC           (3)
+#define EXAMPLE_LCD_PIN_NUM_RGB_HSYNC           (46)
+#define EXAMPLE_LCD_PIN_NUM_RGB_DE              (5)
+#define EXAMPLE_LCD_PIN_NUM_RGB_PCLK            (7)
+#define EXAMPLE_LCD_PIN_NUM_RGB_DATA0           (14)
+#define EXAMPLE_LCD_PIN_NUM_RGB_DATA1           (38)
+#define EXAMPLE_LCD_PIN_NUM_RGB_DATA2           (18)
+#define EXAMPLE_LCD_PIN_NUM_RGB_DATA3           (17)
+#define EXAMPLE_LCD_PIN_NUM_RGB_DATA4           (10)
+#define EXAMPLE_LCD_PIN_NUM_RGB_DATA5           (39)
+#define EXAMPLE_LCD_PIN_NUM_RGB_DATA6           (0)
+#define EXAMPLE_LCD_PIN_NUM_RGB_DATA7           (45)
+#if EXAMPLE_LCD_RGB_DATA_WIDTH > 8
+#define EXAMPLE_LCD_PIN_NUM_RGB_DATA8           (45)
+#define EXAMPLE_LCD_PIN_NUM_RGB_DATA9           (47)
+#define EXAMPLE_LCD_PIN_NUM_RGB_DATA10          (21)
+#define EXAMPLE_LCD_PIN_NUM_RGB_DATA11          (1)
+#define EXAMPLE_LCD_PIN_NUM_RGB_DATA12          (2)
+#define EXAMPLE_LCD_PIN_NUM_RGB_DATA13          (42)
+#define EXAMPLE_LCD_PIN_NUM_RGB_DATA14          (41)
+#define EXAMPLE_LCD_PIN_NUM_RGB_DATA15          (40)
+#endif
+#define EXAMPLE_LCD_PIN_NUM_RST                 (-1)
+#define EXAMPLE_LCD_PIN_NUM_BK_LIGHT            (45)
+#define EXAMPLE_LCD_BK_LIGHT_ON_LEVEL           (1)
+
+#define EXAMPLE_LCD_BK_LIGHT_OFF_LEVEL !EXAMPLE_LCD_BK_LIGHT_ON_LEVEL
+
+/* Enable or disable printing RGB refresh rate */
+#define EXAMPLE_ENABLE_PRINT_LCD_FPS            (1)
+
+#define _EXAMPLE_LCD_CLASS(name, ...) ESP_PanelLcd_##name(__VA_ARGS__)
+#define EXAMPLE_LCD_CLASS(name, ...)  _EXAMPLE_LCD_CLASS(name, ##__VA_ARGS__)
+
+#if EXAMPLE_ENABLE_PRINT_LCD_FPS
+#define EXAMPLE_LCD_FPS_COUNT_MAX               (100)
+
+DRAM_ATTR int frame_count = 0;
+DRAM_ATTR int fps = 0;
+DRAM_ATTR long start_time = 0;
+
+IRAM_ATTR bool onVsyncEndCallback(void *user_data)
+{
+    long frame_start_time = *(long *)user_data;
+    if (frame_start_time == 0) {
+        (*(long *)user_data) = millis();
+
+        return false;
+    }
+
+    frame_count++;
+    if (frame_count >= EXAMPLE_LCD_FPS_COUNT_MAX) {
+        fps = EXAMPLE_LCD_FPS_COUNT_MAX * 1000 / (millis() - frame_start_time);
+        frame_count = 0;
+        (*(long *)user_data) = millis();
+    }
+
+    return false;
+}
+#endif
 
 void setup()
 {
-    String title = "LVGL porting example";
-
     Serial.begin(115200);
-    Serial.println(title + " start");
+    Serial.println("RGB LCD example start");
 
-    Serial.println("Initialize panel device");
-    ESP_Panel *panel = new ESP_Panel();
-    panel->init();
-#if LVGL_PORT_AVOID_TEAR
-    // When avoid tearing function is enabled, configure the RGB bus according to the LVGL configuration
-    ESP_PanelBus_RGB *rgb_bus = static_cast<ESP_PanelBus_RGB *>(panel->getLcd()->getBus());
-    rgb_bus->configRgbFrameBufferNumber(LVGL_PORT_DISP_BUFFER_NUM);
-    rgb_bus->configRgbBounceBufferSize(LVGL_PORT_RGB_BOUNCE_BUFFER_SIZE);
+#if EXAMPLE_LCD_PIN_NUM_BK_LIGHT >= 0
+    Serial.println("Initialize backlight control pin and turn it off");
+    ESP_PanelBacklight *backlight = new ESP_PanelBacklight(EXAMPLE_LCD_PIN_NUM_BK_LIGHT, EXAMPLE_LCD_BK_LIGHT_ON_LEVEL, true);
+    backlight->begin();
+    backlight->off();
 #endif
-    panel->begin();
 
-    Serial.println("Initialize LVGL");
-    lvgl_port_init(panel->getLcd(), panel->getTouch());
+    Serial.println("Create RGB LCD bus");
+#if EXAMPLE_LCD_RGB_DATA_WIDTH == 8
+    ESP_PanelBus_RGB *panel_bus = new ESP_PanelBus_RGB(EXAMPLE_LCD_WIDTH, EXAMPLE_LCD_HEIGHT,
+                                                       EXAMPLE_LCD_PIN_NUM_RGB_DATA0, EXAMPLE_LCD_PIN_NUM_RGB_DATA1,
+                                                       EXAMPLE_LCD_PIN_NUM_RGB_DATA2, EXAMPLE_LCD_PIN_NUM_RGB_DATA3,
+                                                       EXAMPLE_LCD_PIN_NUM_RGB_DATA4, EXAMPLE_LCD_PIN_NUM_RGB_DATA5,
+                                                       EXAMPLE_LCD_PIN_NUM_RGB_DATA6, EXAMPLE_LCD_PIN_NUM_RGB_DATA7,
+                                                       EXAMPLE_LCD_PIN_NUM_RGB_HSYNC, EXAMPLE_LCD_PIN_NUM_RGB_VSYNC,
+                                                       EXAMPLE_LCD_PIN_NUM_RGB_PCLK, EXAMPLE_LCD_PIN_NUM_RGB_DE,
+                                                       EXAMPLE_LCD_PIN_NUM_RGB_DISP);
+#elif EXAMPLE_LCD_RGB_DATA_WIDTH == 16
+    ESP_PanelBus_RGB *panel_bus = new ESP_PanelBus_RGB(EXAMPLE_LCD_WIDTH, EXAMPLE_LCD_HEIGHT,
+                                                       EXAMPLE_LCD_PIN_NUM_RGB_DATA0, EXAMPLE_LCD_PIN_NUM_RGB_DATA1,
+                                                       EXAMPLE_LCD_PIN_NUM_RGB_DATA2, EXAMPLE_LCD_PIN_NUM_RGB_DATA3,
+                                                       EXAMPLE_LCD_PIN_NUM_RGB_DATA4, EXAMPLE_LCD_PIN_NUM_RGB_DATA5,
+                                                       EXAMPLE_LCD_PIN_NUM_RGB_DATA6, EXAMPLE_LCD_PIN_NUM_RGB_DATA7,
+                                                       EXAMPLE_LCD_PIN_NUM_RGB_DATA8, EXAMPLE_LCD_PIN_NUM_RGB_DATA9,
+                                                       EXAMPLE_LCD_PIN_NUM_RGB_DATA10, EXAMPLE_LCD_PIN_NUM_RGB_DATA11,
+                                                       EXAMPLE_LCD_PIN_NUM_RGB_DATA12, EXAMPLE_LCD_PIN_NUM_RGB_DATA13,
+                                                       EXAMPLE_LCD_PIN_NUM_RGB_DATA14, EXAMPLE_LCD_PIN_NUM_RGB_DATA15,
+                                                       EXAMPLE_LCD_PIN_NUM_RGB_HSYNC, EXAMPLE_LCD_PIN_NUM_RGB_VSYNC,
+                                                       EXAMPLE_LCD_PIN_NUM_RGB_PCLK, EXAMPLE_LCD_PIN_NUM_RGB_DE,
+                                                       EXAMPLE_LCD_PIN_NUM_RGB_DISP);
+#endif
+    panel_bus->configRgbTimingFreqHz(EXAMPLE_LCD_RGB_TIMING_FREQ_HZ);
+    panel_bus->configRgbTimingPorch(EXAMPLE_LCD_RGB_TIMING_HPW, EXAMPLE_LCD_RGB_TIMING_HBP, EXAMPLE_LCD_RGB_TIMING_HFP,
+                                    EXAMPLE_LCD_RGB_TIMING_VPW, EXAMPLE_LCD_RGB_TIMING_VBP, EXAMPLE_LCD_RGB_TIMING_VFP);
+    // panel_bus->configRgbBounceBufferSize(EXAMPLE_LCD_WIDTH * 10); // Set bounce buffer to avoid screen drift
+    panel_bus->begin();
 
-    Serial.println("Create UI");
-    /* Lock the mutex due to the LVGL APIs are not thread-safe */
-    lvgl_port_lock(-1);
+    Serial.println("Create LCD device");
+    ESP_PanelLcd *lcd = new EXAMPLE_LCD_CLASS(EXAMPLE_LCD_NAME, panel_bus, EXAMPLE_LCD_COLOR_BITS, EXAMPLE_LCD_PIN_NUM_RST);
+    lcd->init();
+    lcd->reset();
+    lcd->begin();
+#if EXAMPLE_LCD_PIN_NUM_RGB_DISP >= 0
+    lcd->displayOn();
+#endif
+#if EXAMPLE_ENABLE_PRINT_LCD_FPS
+    lcd->attachRefreshFinishCallback(onVsyncEndCallback, (void *)&start_time);
+#endif
 
-    /* Create a simple label */
-    lv_obj_t *label = lv_label_create(lv_scr_act());
-    lv_label_set_text(label, title.c_str());
-    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+    Serial.println("Draw color bar from top left to bottom right, the order is B - G - R");
+    lcd->colorBarTest(EXAMPLE_LCD_WIDTH, EXAMPLE_LCD_HEIGHT);
 
-    /**
-     * Try an example. Don't forget to uncomment header.
-     * See all the examples online: https://docs.lvgl.io/master/examples.html
-     * source codes: https://github.com/lvgl/lvgl/tree/e7f88efa5853128bf871dde335c0ca8da9eb7731/examples
-     */
-      lv_example_btn_1();
+#if EXAMPLE_LCD_PIN_NUM_BK_LIGHT >= 0
+    Serial.println("Turn on the backlight");
+    backlight->on();
+#endif
 
-    /**
-     * Or try out a demo.
-     * Don't forget to uncomment header and enable the demos in `lv_conf.h`. E.g. `LV_USE_DEMOS_WIDGETS`
-     */
-    // lv_demo_widgets();
-    // lv_demo_benchmark();
-    // lv_demo_music();
-    // lv_demo_stress();
-
-    /* Release the mutex */
-    lvgl_port_unlock();
-
-    Serial.println(title + " end");
+    Serial.println("RGB LCD example end");
 }
 
 void loop()
 {
-    Serial.println("IDLE loop");
     delay(1000);
+#if EXAMPLE_ENABLE_PRINT_LCD_FPS
+    Serial.println("RGB refresh rate: " + String(fps));
+#else
+    Serial.println("IDLE loop");
+#endif
 }
