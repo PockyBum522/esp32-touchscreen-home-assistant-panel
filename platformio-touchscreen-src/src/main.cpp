07fc81e6e1b33aee14fb54b4ba9af07d668160cf
David S <1970959+PockyBum522@users.noreply.github.com>
2025-02-18 12:54:22 -0500

    adding wifi reconnect, beginning cleaning things up

--- platformio-touchscreen-src/src/main.cpp
+++ platformio-touchscreen-src/src/main.cpp
@@ -27,6 +27,13 @@ auto panel = new ESP_Panel();
 #include "SetupHelpers/LvglInitializer.h"
 #include "SetupHelpers/NetworkHandlers.h"
 
+	// TODO: If touchscreen connection is reliable for a while, re-add OTA and see if that can be gotten working
+
+	// TODO: If that works, see if you can download LVGL config from the network on startup and just render it
+
+	// TODO: Add camera stills?
+	// TODO: Add back porch lights, laundry room lights, cat last pilled, washer status
+
 void setup()
 {
     Serial.begin(115200);
@@ -39,30 +46,36 @@ void setup()
     TouchscreenHardwareInitializer::InitLcdTouchscreenHardware();
     LvglInitializer::InitLvglToTouchscreen();
 
-    NetworkHandlers::ConnectWifi();
-    NetworkHandlers::SetupOtaServer();
+	NetworkHandlers::ConnectWifi();
+	//NetworkHandlers::SetupOtaServer();
 
     delay(1000);
     Serial.println("Finished setup(), starting loop() - " + m_versionNumber);
 }
 
+void reconnectNetworksIfDisconnected();
+void sendMqttHeartbeatEvery20Seconds();
+
 void loop()
 {
-    // All of these checks are just to make it so if something takes a long time, don't run the others until loop()
-    //      has finished. I don't know if LVGL runs things in between loop() but I figure it can't hurt.
-    //      ...I mean it can, but...
+	reconnectNetworksIfDisconnected();
 
-	if (!m_mqttClient.connected())
-	{
-		NetworkHandlers::ConnectMqtt();
-	}
+	m_mqttClient.loop();
+
+	sendMqttHeartbeatEvery20Seconds();
+
+	// TODO: Once connection is reliable, clean all this mess up
 
     // MQTT
-    long long before = millis();
-    m_mqttClient.loop();
-    const long long afterMqtt = millis() - before;
+	//    long long before = millis();
+	//
+	// const long long afterMqtt = millis() - before;
 
-    // // OTA
+	// All of these checks are just to make it so if something takes a long time, don't run the others until loop()
+	//      has finished. I don't know if LVGL runs things in between loop() but I figure it can't hurt.
+	//      ...I mean it can, but...
+
+	// // OTA
     // before = millis();
     //
     // // if (afterMqtt < 10)
@@ -89,15 +102,7 @@ void loop()
     //     ESP.restart();
     // }
 
-    // Send heartbeat message every 20 seconds
-    if (m_sinceLastHeartbeatMessage > 20)
-    {
-        m_mqttClient.publish(SECRETS::MqttTopicDeviceStatus, ("heartbeat_wd_" + m_versionNumber).c_str());
 
-        Serial.println(("heartbeat_wd_" + m_versionNumber).c_str());
-
-        m_sinceLastHeartbeatMessage = 0;
-    }
 
     // Don't do anything beyond here if we don't have debug flag on
     if (!m_debugSerialOn) return;
@@ -115,5 +120,33 @@ void loop()
     //     Serial.println(afterMqtt);
     //     Serial.println();
     // }
+}
+
+void reconnectNetworksIfDisconnected()
+{
+	// Reconnect to Wi-Fi if disconnected
+	if (WiFi.status() != WL_CONNECTED)
+	{
+		NetworkHandlers::ConnectWifi();
+	}
+
+	// Reconnect to MQTT broker if disconnected
+	if (!m_mqttClient.connected())
+	{
+		NetworkHandlers::ConnectMqtt();
+	}
+}
+
+void sendMqttHeartbeatEvery20Seconds()
+{
+	// Send heartbeat message every 20 seconds
+	if (m_sinceLastHeartbeatMessage > 20)
+	{
+		m_mqttClient.publish(SECRETS::MqttTopicDeviceStatus, ("heartbeat_wd_" + m_versionNumber).c_str());
+
+		Serial.println(("heartbeat_wd_" + m_versionNumber).c_str());
+
+		m_sinceLastHeartbeatMessage = 0;
+	}
 }
 
